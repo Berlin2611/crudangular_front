d5616837ba69b1b59c5fef6082262982c1c301ed
Berlin2611 <juli.mora.5667@gmail.com>
2020-03-19 00:13:33 -0500

    Version 1

---
+++ src/app/Persona/add/add.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ServiceService } from 'src/app/Service/service.service';
+import { Persona } from 'src/app/Modelo/Persona';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+  per:Persona = new Persona();
+  constructor(private router:Router, private service:ServiceService) { }
+
+  ngOnInit(){
+  }
+
+  Registrar(nombre:String, apellidos:String){
+    this.per.nombres = nombre;
+    this.per.apellidos = apellidos;
+    this.service.createPersona(this.per)
+    .subscribe(data=>{
+      alert("Se agregó con éxito.");
+      this.router.navigate(["listar"]);
+    })
+  }
+}
